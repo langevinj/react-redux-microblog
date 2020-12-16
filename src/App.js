b133ce9b3de776bdde416e18a929e9387fa58c9a
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 14:15:10 -0500

    Added skeleton of Blog and Header component, started filling out App component routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './Header'
+import { Route } from 'react-router-dom'
+import Blog from './Blog'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Route exact path="/">
+        <Blog />
+      </Route>
     </div>
   );
 }
