264e79f482ae243eac9dd9530e1c9baaa33cb246
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 15:06:22 -0500

    BlogContext created

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import './App.css';
 import Header from './Header'
 import NewPost from './NewPost'
 import Post from './Post'
-import { Route } from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom'
 import Blog from './Blog'
+import BlogContext from './BlogContext'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Route exact path="/new">
-        <NewPost />
-      </Route>
-      <Route exact path="/:postid">
-        <Post />
-      </Route>
-      <Route exact path="/">
-        <Blog />
-      </Route>
-    </div>
+    <BrowserRouter>
+      <BlogContext.Provider value={{ blogs, setBlogs }}>
+        <div className="App">
+          <Header />
+          <Route exact path="/new">
+            <NewPost />
+          </Route>
+          <Route exact path="/:postid">
+            <Post />
+          </Route>
+          <Route exact path="/">
+            <Blog />
+          </Route>
+        </div>
+      </BlogContext.Provider>
+    </BrowserRouter>
   );
 }
 
