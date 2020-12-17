1f3c24e35820ed7536f36a93914d466025c3c27d
Jeremy Langevin <langevinj832@gmail.com>
2020-12-17 13:28:24 -0500

    Updated index.js to account for thunk

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from 'redux-thunk'
 import reducer from './reducer'
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__
+        && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}>
