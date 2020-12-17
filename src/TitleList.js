d7099609f6deda20f32821b8669cee148bed7549
Jeremy Langevin <langevinj832@gmail.com>
2020-12-17 13:37:46 -0500

    TitleList temporary is using the API for posts

--- src/TitleList.js
+++ src/TitleList.js
@@ -1,9 +1,21 @@
-import React from 'react' 
+import React, {useEffect} from 'react' 
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchPostsFromApi } from './actionCreators';
 
 function TitleList(){
     const posts = useSelector(st => st.posts);
+    const error = useSelector(st => st.error);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPostsFromApi());
+    }, [dispatch])
+
+    if(error) {
+        return <h3>Something is wrong...</h3>
+    }
+
 
     return (
         <ul>
