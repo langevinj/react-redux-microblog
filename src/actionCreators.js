855a399ddbacfd9f061d80dca4f36629b93c7abc
Jeremy Langevin <langevinj832@gmail.com>
2020-12-17 13:26:33 -0500

    Added beginnings of actionsCreators

---
+++ src/actionCreators.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/api'
+
+function fetchPosts(posts) {
+    return {
+        type: 'FETCH_POSTS',
+        posts
+    };
+}
+
+function handleError(error) {
+    return {
+        type: 'ERROR',
+        error
+    };
+}
+
+export function fetchPostsFromApi() {
+    return async function thunk(dispatch) {
+        try {
+            let posts = await axios.get(`${API_URL}/posts`);
+            dispatch(fetchPosts(posts.data));
+        } catch (error) {
+            dispatch(handleError(error.response.data));
+        }
+    }
+}
