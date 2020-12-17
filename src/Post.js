363e4104b566d7c0c0a23a5998b90948b41e47ac
Jeremy Langevin <langevinj832@gmail.com>
2020-12-17 11:34:09 -0500

    Post component rewritten to use PostDisplay

--- src/Post.js
+++ src/Post.js
@@ -3,54 +3,68 @@ import { useParams, useHistory } from 'react-router-dom'
 import BlogContext from './BlogContext' 
 import NewPost from './NewPost'
 import CommentForm from './CommentForm'
+import PostDisplay from './PostDisplay'
+import { useSelector, useDispatch } from 'react-redux'
 
 function Post() {
     const { id } = useParams();
-    const [post, setPost] = useState(null)
-    const { blogs, setBlogs } = useContext(BlogContext)
+    const posts = useSelector(st => st.posts)
+    //set the current post to the post from the id
+    const currentPost = posts ? posts[id] : null
     const [editView, setEditView] = useState(false)
     const history = useHistory()
 
-    useEffect(() => {
-        function loadBlog(){
-            if(blogs){
-                let targetPost = blogs.filter(b => b.id === `${id}`)
-                setPost(targetPost[0])
-            }
-        }
-        loadBlog()
-    }, [])
-    
-    if(!post) { return <h1>404 oops this blog post wasn't found!</h1>}
+    if (!currentPost) { return <h1>404 oops this blog post wasn't found!</h1> }
 
     const toggleEditView = (evt) => {
         setEditView(!editView)
     }
 
-    const deleteBlog = (evt) => {
-        evt.preventDefault();
-        if(blogs){
-            console.log(blogs)
-            setBlogs(blogs.filter(b => b.id !== `${evt.target.parentNode.id}`))
-        }
-        history.push("/")
-    }
+    // const [post, setPost] = useState(null)
+    // const { blogs, setBlogs } = useContext(BlogContext)
+    
 
-    const deleteComment = (evt) => {
-        evt.preventDefault();
-        setBlogs(post.comments.length > 0 ? blogs.map(blog => (blog.id !== `${evt.target.parentNode.id}` ? blog : { ...blog, comments: blog.comments.filter(c => c.id !== `${evt.target.id}`)})) : blogs)
-        //kind of hacky way to remove the button but it works
-        evt.target.parentNode.remove();
-    }
+    // useEffect(() => {
+    //     function loadBlog(){
+    //         if(blogs){
+    //             let targetPost = blogs.filter(b => b.id === `${id}`)
+    //             setPost(targetPost[0])
+    //         }
+    //     }
+    //     loadBlog()
+    // }, [])
+    
+    
 
+    
+
+    // const deleteBlog = (evt) => {
+    //     evt.preventDefault();
+    //     if(blogs){
+    //         console.log(blogs)
+    //         setBlogs(blogs.filter(b => b.id !== `${evt.target.parentNode.id}`))
+    //     }
+    //     history.push("/")
+    // }
+
+    // const deleteComment = (evt) => {
+    //     evt.preventDefault();
+    //     setBlogs(post.comments.length > 0 ? blogs.map(blog => (blog.id !== `${evt.target.parentNode.id}` ? blog : { ...blog, comments: blog.comments.filter(c => c.id !== `${evt.target.id}`)})) : blogs)
+    //     //kind of hacky way to remove the button but it works
+    //     evt.target.parentNode.remove();
+    // }
+
+    const deleteComment = () => {}
+    const deleteBlog = () => {}
     return (
         <div>
             {!editView ? (<>
-                <PostDisplay post={currentPost}/>
+                <PostDisplay post={currentPost} toggleEditView={toggleEditView} deleteBlog={deleteBlog}/>
                 <h3 className="border-top">Comments:</h3>
-                {post.comments ? post.comments.map(comment => <div key={comment.id} id={post.id}><button className="btn fas fa-times text-danger" onClick={deleteComment} id={comment.id}></button><p>{comment.text}</p></div>) : <p className="text-secondary font-italic">No comments on this post yet</p>}
-                {post.id ? <CommentForm post={post} /> : null}
-            </>) : <NewPost post={post}/>}
+                {currentPost.comments ? currentPost.comments.map(comment => <div key={comment.id} id={currentPost.id}><button className="btn fas fa-times text-danger" onClick={deleteComment} id={comment.id}></button><p>{comment.text}</p></div>) : <p className="text-secondary font-italic">No comments on this post yet</p>}</>) : null}
+                {/* {post.id ? <CommentForm post={post} /> : null}
+            </>) : <NewPost post={post}/>} */}
+        
         </div>
     )
 }
--- src/PostDisplay.js
+++ src/PostDisplay.js
@@ -1,16 +1,15 @@
 import React from 'react' 
 
-function PostDisplay({ post }) {
-
+function PostDisplay({ post = null, toggleEditView = () => {}, deleteBlog = () => {} }) {
     return (
         <>
-        <div className="post-header" id={post.id}>
+        {post ? (<><div className="post-header" id={post.id}>
                 <h2 className="title">{post.title}</h2>
                 <button className="btn fas fa-edit text-primary mr-3" onClick={toggleEditView}></button>
                 <button className="btn fas fa-times text-danger" onClick={deleteBlog}></button>
         </div>
             <p className="font-italic">{post.description}</p>
-            <p>{post.body}</p>
+            <p>{post.body}</p></>) : null}
         </>
     )
 }
