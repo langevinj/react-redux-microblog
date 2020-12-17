382e4dae7f2300e425b21162ff77e76419258599
Jeremy Langevin <langevinj832@gmail.com>
2020-12-17 10:43:58 -0500

    added useSelector and refactored TitleList component

--- src/TitleList.js
+++ src/TitleList.js
@@ -1,11 +1,12 @@
 import React from 'react' 
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-function TitleList({ blogs = null }){
-    
+function TitleList(){
+    const posts = useSelector(st => st.posts)
     return (
         <ul>
-            {blogs ? blogs.map(blog => <li className="blogCard container w-25 border rounded bg-light m-2 d-inline-block p-3"><Link to={`/posts/${blog.id}`}><p>{blog.title}</p></Link><p>{blog.description}</p></li>) : null}
+            {posts ? Object.entries(posts).map((id, post) => <li className="postCard container w-25 border rounded bg-light m-2 d-inline-block p-3"><Link to={`/posts/${id}`}><p>{post.title}</p></Link><p>{post.description}</p></li>) : null}
         </ul>
     )
 }
