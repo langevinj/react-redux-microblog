565850b862952a79170a42925f5b432675f06e80
Jeremy Langevin <langevinj832@gmail.com>
2020-12-17 07:54:41 -0500

    added edit form toggler to Post component

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import BlogContext from './BlogContext' 
+import NewPost from './NewPost'
 
 function Post() {
     const { id } = useParams();
     const [post, setPost] = useState(null)
     const { blogs } = useContext(BlogContext)
+    const [editView, setEditView] = useState(false)
     
     useEffect(() => {
         function loadBlog(){
@@ -17,11 +19,21 @@ function Post() {
     
     if(!post) { return <h1>404 oops this blog post wasn't found!</h1>}
 
+    const toggleEditView = (evt) => {
+        setEditView(!editView)
+    }
+
     return (
         <div>
-            <h2 className="title">{post.title}</h2>
-            <p className="font-italic">{post.description}</p>
-            <p>{post.body}</p>
+            {!editView ? (<>
+                <div className="post-header">
+                    <h2 className="title">{post.title}</h2>
+                    <button className="btn" onClick={toggleEditView}><i class="fas fa-edit text-primary mr-3"></i></button>
+                    <button className="btn"><i class="fas fa-times text-danger"></i></button>
+                </div>
+                <p className="font-italic">{post.description}</p>
+                <p>{post.body}</p>
+            </>) : <NewPost post={post}/>}
         </div>
     )
 }
