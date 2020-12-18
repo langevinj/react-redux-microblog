e33ee3d3294e74e24becf44b7a0cf63466895df3
Jeremy Langevin <langevinj832@gmail.com>
2020-12-18 07:11:29 -0500

    Added unique keys to TitleList component

--- src/TitleList.js
+++ src/TitleList.js
@@ -1,13 +1,14 @@
 import React from 'react' 
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import {v4 as uuid} from 'uuid'
 
 function TitleList(){
     const posts = useSelector(st => st.posts);
 
     return (
         <ul>
-            {posts ? Object.entries(posts).map(([id, post]) => <li className="postCard container w-25 border rounded bg-light m-2 d-inline-block p-3"><Link to={`/posts/${id}`}><p>{post.title}</p></Link><p>{post.description}</p></li>) : null}
+            {posts ? Object.entries(posts).map(([id, post]) => <li className="postCard container w-25 border rounded bg-light m-2 d-inline-block p-3" key={uuid()}><Link to={`/posts/${id}`}><p>{post.title}</p></Link><p>{post.description}</p></li>) : null}
         </ul>
     )
 }
