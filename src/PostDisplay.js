6ee8f3c9913790ea8327b6217d032910e24c7954
Jeremy Langevin <langevinj832@gmail.com>
2020-12-18 09:50:19 -0500

    added vote count

--- src/PostDisplay.js
+++ src/PostDisplay.js
@@ -8,7 +8,7 @@ function PostDisplay({ post = null, toggleEditView = () => {}, deleteBlog = () =
                 <h2 className="title">{post.title}</h2>
                 <button className="btn fas fa-edit text-primary mr-3" onClick={toggleEditView}></button>
                 <button className="btn fas fa-times text-danger" onClick={deleteBlog}></button>
-                <VotingButtons />
+                <VotingButtons count={post.votes}/>
         </div>
             <p className="font-italic">{post.description}</p>
                 <p>{post.body}</p></>) : <h1>404 oops this blog post wasn't found!</h1>}
--- src/VotingButtons.js
+++ src/VotingButtons.js
@@ -1,10 +1,10 @@
 import React from 'react' 
 
-function VotingButtons() {
+function VotingButtons({ count = 0 }) {
 
     return (
         <div className="voting-buttons">
-            <span className="vote-count font-weight-bold">votes: </span>
+            <span className="vote-count font-weight-bold mr-2">votes: {count}</span>
             <button className="far fa-thumbs-up btn btn-success mr-3"></button>
             <button className="far fa-thumbs-down btn btn-danger"></button>
         </div>
